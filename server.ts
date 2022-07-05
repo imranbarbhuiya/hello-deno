e8aabd74ac9110dc2ede7ae8b8388e2c2577b8e3
parbez <imranbarbhuiya.fsd@gmail.com>
2022-07-05 12:11:10 +0530

    deno fmt

--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 const listener = Deno.listen({ port: 8000 });
-console.log('http://localhost:8000/');
+console.log("http://localhost:8000/");
 
 for await (const conn of listener) {
   serve(conn);
@@ -7,6 +7,6 @@ for await (const conn of listener) {
 
 async function serve(conn: Deno.Conn) {
   for await (const { respondWith } of Deno.serveHttp(conn)) {
-    respondWith(new Response('Hello world'));
+    respondWith(new Response("Hello world"));
   }
 }
